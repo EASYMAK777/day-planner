55e20d8680a8dee55f1f5a196fecbea5bec5606b
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-13 19:24:42 -0400

    added onclick event to save new tasks

--- Assets/moment.js
+++ Assets/moment.js
@@ -8,3 +8,16 @@ var moment = moment();
 var currentDay = moment.format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(currentDay);
 
+var moment = moment();
+var currentDay = moment.format('dddd, MMMM Do, YYYY');
+$("#currentDay").text(currentDay);
+
+//Create onclick events for storage
+
+button.on("click", function () {
+    console.log("hit")
+    var userInput = $(this).siblings(".description").val();
+        var mainRow = $(this).parent().attr("id");
+        localStorage.setItem(mainRow, userInput)
+});
+
