9309c6b5fe37b51a62609e134bec2459e0107a73
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-13 19:42:14 -0400

    added final timerows

--- Assets/moment.js
+++ Assets/moment.js
@@ -1,18 +1,13 @@
 
-
-//create variables to set up moment.js
-var time = moment().format("H")
+//make variables:
+var hour = moment().format("H")
 var button = $(".saveBtn") 
 
 var moment = moment();
 var currentDay = moment.format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(currentDay);
 
-var moment = moment();
-var currentDay = moment.format('dddd, MMMM Do, YYYY');
-$("#currentDay").text(currentDay);
-
-//Create onclick events for storage
+//make on clicks
 
 button.on("click", function () {
     console.log("hit")
@@ -20,8 +15,6 @@ button.on("click", function () {
         var mainRow = $(this).parent().attr("id");
         localStorage.setItem(mainRow, userInput)
 });
-
-//pulls tasks from local storage
 
 $("#9 .description").val(localStorage.getItem("9"))
 $("#10 .description").val(localStorage.getItem("10"))
@@ -33,15 +26,15 @@ $("#15 .description").val(localStorage.getItem("15"))
 $("#16 .description").val(localStorage.getItem("16"))
 $("#17 .description").val(localStorage.getItem("17"))
 
-//this function changes color according to the current hour
 
-function getColors() {
+
+function getSetColors() {
     $(".time-block").each(function () {
         var divId = $(this).attr("id")
         var divTime = parseInt(divId);
-        if(divTime < time) {
+        if(divTime < hour) {
             $(this).children(".description").attr("class", "col-md-10 description past");
-        } else if (divTime == time) {
+        } else if (divTime == hour) {
             $(this).children(".description").attr("class", "col-md-10 description present");
         } else {
             $(this).children(".description").attr("class", "col-md-10 description future");
@@ -50,10 +43,10 @@ function getColors() {
 
 };
 
-getColors()
+getSetColors()
 
 function setTimeInterval() {
-    window.setInterval(getColors, 3000);
+    window.setInterval(getSetColors, 3000);
 };
 
 setTimeInterval();
